813ed267f8f6fcaf7364dd3d4ee606fd08765c18
Abmilro3 <137915955+Abmilro3@users.noreply.github.com>
2023-07-20 13:41:03 -0700

    Python pro 2

---
+++ python-functions/0-sum.py
@@ -0,0 +1,9 @@
+# Function to add two integers and return the result
+def add(a, b):
+    return a + b
+#!/usr/bin/env python3
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
