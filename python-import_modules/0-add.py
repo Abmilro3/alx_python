547aef00fe427f9a383cef33610d541d5ede8a39
Abmilro3 <137915955+Abmilro3@users.noreply.github.com>
2023-07-25 06:10:15 -0700

    Correction

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
-# Import the 'add' function from 'add_0.py'
+#!/usr/bin/python3
+
 from add_0 import add
 
-# Assign values to variables 'a' and 'b'
 a = 1
 b = 2
 
-# Use the 'add' function to add 'a' and 'b', and print the result
-print("{} + {} = {}".format(a, b, add(a, b)))
+# Using string format to display integers
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
