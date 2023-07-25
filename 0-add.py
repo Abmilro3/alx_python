bdceb347da1caf14260cc33c25b7f64d046dc751
Abmilro3 <137915955+Abmilro3@users.noreply.github.com>
2023-07-25 02:46:15 -0700

    Delete 0-add.py

--- 0-add.py
+++
@@ -1,9 +0,0 @@
-# Import the 'add' function from 'add_0.py'
-from add_0 import add
-
-# Assign values to variables 'a' and 'b'
-a = 1
-b = 2
-
-# Use the 'add' function to add 'a' and 'b', and print the result
-print("{} + {} = {}".format(a, b, add(a, b)))
